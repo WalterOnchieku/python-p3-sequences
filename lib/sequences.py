cf5e9e4b21c894b051fadd6bba0dcf40ee803e93
root <root@DESKTOP-RHA5CJR.localdomain>
2024-09-01 01:44:35 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    elif length == 3:
+        print([0, 1, 1])
+    else:
+        sequence = print_fibonacci(length - 1)
+        sequence.append(sequence[-1] + sequence[-2])
+        return sequence
+    
